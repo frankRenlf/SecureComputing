6890aad7c9621012c244185a3e25afc2134e1f4f
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-11-07 21:24:08 +0800

    update

--- cw2/patients/src/main/java/comp3911/cwk2/AppServlet.java
+++ cw2/patients/src/main/java/comp3911/cwk2/AppServlet.java
@@ -25,107 +25,103 @@ import freemarker.template.TemplateExceptionHandler;
 @SuppressWarnings("serial")
 public class AppServlet extends HttpServlet {
 
-  private static final String CONNECTION_URL = "jdbc:sqlite:db.sqlite3";
-  private static final String AUTH_QUERY = "select * from user where username='%s' and password='%s'";
-  private static final String SEARCH_QUERY = "select * from patient where surname like '%s'";
+    private static final String CONNECTION_URL = "jdbc:sqlite:db.sqlite3";
+    private static final String AUTH_QUERY = "select * from user where username='%s' and password='%s'";
+    // ' or 1==1 or '
+    private static final String SEARCH_QUERY = "select * from patient where surname like '%s'";
 
-  private final Configuration fm = new Configuration(Configuration.VERSION_2_3_28);
-  private Connection database;
+    private final Configuration fm = new Configuration(Configuration.VERSION_2_3_28);
+    private Connection database;
 
-  @Override
-  public void init() throws ServletException {
-    configureTemplateEngine();
-    connectToDatabase();
-  }
+    @Override
+    public void init() throws ServletException {
+        configureTemplateEngine();
+        connectToDatabase();
+    }
 
-  private void configureTemplateEngine() throws ServletException {
-    try {
-      fm.setDirectoryForTemplateLoading(new File("./templates"));
-      fm.setDefaultEncoding("UTF-8");
-      fm.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
-      fm.setLogTemplateExceptions(false);
-      fm.setWrapUncheckedExceptions(true);
+    private void configureTemplateEngine() throws ServletException {
+        try {
+            fm.setDirectoryForTemplateLoading(new File("./templates"));
+            fm.setDefaultEncoding("UTF-8");
+            fm.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+            fm.setLogTemplateExceptions(false);
+            fm.setWrapUncheckedExceptions(true);
+        } catch (IOException error) {
+            throw new ServletException(error.getMessage());
+        }
     }
-    catch (IOException error) {
-      throw new ServletException(error.getMessage());
-    }
-  }
 
-  private void connectToDatabase() throws ServletException {
-    try {
-      database = DriverManager.getConnection(CONNECTION_URL);
+    private void connectToDatabase() throws ServletException {
+        try {
+            database = DriverManager.getConnection(CONNECTION_URL);
+        } catch (SQLException error) {
+            throw new ServletException(error.getMessage());
+        }
     }
-    catch (SQLException error) {
-      throw new ServletException(error.getMessage());
-    }
-  }
 
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-   throws ServletException, IOException {
-    try {
-      Template template = fm.getTemplate("login.html");
-      template.process(null, response.getWriter());
-      response.setContentType("text/html");
-      response.setStatus(HttpServletResponse.SC_OK);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            Template template = fm.getTemplate("login.html");
+            template.process(null, response.getWriter());
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+        } catch (TemplateException error) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
     }
-    catch (TemplateException error) {
-      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-    }
-  }
 
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-   throws ServletException, IOException {
-     // Get form parameters
-    String username = request.getParameter("username");
-    String password = request.getParameter("password");
-    String surname = request.getParameter("surname");
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Get form parameters
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String surname = request.getParameter("surname");
 
-    try {
-      if (authenticated(username, password)) {
-        // Get search results and merge with template
-        Map<String, Object> model = new HashMap<>();
-        model.put("records", searchResults(surname));
-        Template template = fm.getTemplate("details.html");
-        template.process(model, response.getWriter());
-      }
-      else {
-        Template template = fm.getTemplate("invalid.html");
-        template.process(null, response.getWriter());
-      }
-      response.setContentType("text/html");
-      response.setStatus(HttpServletResponse.SC_OK);
+        try {
+            if (authenticated(username, password)) {
+                // Get search results and merge with template
+                Map<String, Object> model = new HashMap<>();
+                model.put("records", searchResults(surname));
+                Template template = fm.getTemplate("details.html");
+                template.process(model, response.getWriter());
+            } else {
+                Template template = fm.getTemplate("invalid.html");
+                template.process(null, response.getWriter());
+            }
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+        } catch (Exception error) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
     }
-    catch (Exception error) {
-      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-    }
-  }
 
-  private boolean authenticated(String username, String password) throws SQLException {
-    String query = String.format(AUTH_QUERY, username, password);
-    try (Statement stmt = database.createStatement()) {
-      ResultSet results = stmt.executeQuery(query);
-      return results.next();
+    private boolean authenticated(String username, String password) throws SQLException {
+        String query = String.format(AUTH_QUERY, username, password);
+        try (Statement stmt = database.createStatement()) {
+            ResultSet results = stmt.executeQuery(query);
+            return results.next();
+        }
     }
-  }
 
-  private List<Record> searchResults(String surname) throws SQLException {
-    List<Record> records = new ArrayList<>();
-    String query = String.format(SEARCH_QUERY, surname);
-    try (Statement stmt = database.createStatement()) {
-      ResultSet results = stmt.executeQuery(query);
-      while (results.next()) {
-        Record rec = new Record();
-        rec.setSurname(results.getString(2));
-        rec.setForename(results.getString(3));
-        rec.setAddress(results.getString(4));
-        rec.setDateOfBirth(results.getString(5));
-        rec.setDoctorId(results.getString(6));
-        rec.setDiagnosis(results.getString(7));
-        records.add(rec);
-      }
+    private List<Record> searchResults(String surname) throws SQLException {
+        List<Record> records = new ArrayList<>();
+        String query = String.format(SEARCH_QUERY, surname);
+        try (Statement stmt = database.createStatement()) {
+            ResultSet results = stmt.executeQuery(query);
+            while (results.next()) {
+                Record rec = new Record();
+                rec.setSurname(results.getString(2));
+                rec.setForename(results.getString(3));
+                rec.setAddress(results.getString(4));
+                rec.setDateOfBirth(results.getString(5));
+                rec.setDoctorId(results.getString(6));
+                rec.setDiagnosis(results.getString(7));
+                records.add(rec);
+            }
+        }
+        return records;
     }
-    return records;
-  }
 }
